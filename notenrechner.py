4ca6ec87c8a776c4620fc253fb4cbbb9dec974e3
Nikola Andonov <nikola.andonov@avenga.com>
2024-09-26 10:12:44 +0200

    update notenrechner and tests

--- notenrechner.py
+++ notenrechner.py
@@ -1,18 +1,30 @@
-def berechne_note(punkte: int):
-    if not isinstance(punkte, (int, float)):
-        raise ValueError("Es muss eine Zahl sein.")
-    if punkte < 0 or punkte > 100:
-        raise ValueError("Die Punkte können nicht unter 0 oder über 100 sein.")
+def berechne_note(punkte: int, max_punkte: int):
+    if not isinstance(punkte, (int, float)) or not isinstance(max_punkte, (int, float)):
+        raise ValueError("Es müssen Zahlen sein.")
+    if punkte < 0 or punkte > max_punkte:
+        raise ValueError("Die Punkte müssen zwischen 0 und den maximalen Punkten liegen.")
+    if max_punkte <= 0:
+        raise ValueError("Die maximalen Punkte müssen größer als 0 sein.")
     
-    if punkte >= 92 and punkte <= 100:
+    prozent = (punkte / max_punkte) * 100
+    
+    return prozent
+    
+def berechne_note_prozent(prozent: int):
+    if not isinstance(prozent, (int, float)):
+        raise ValueError("Es muss eine Zahl sein.")
+    if prozent < 0 or prozent > 100:
+        raise ValueError("Die Prozentzahl muss zwischen 0 und 100 liegen.")
+    
+    if prozent >= 92:
         return 'sehr gut'
-    elif punkte >= 81:
+    elif prozent >= 81:
         return 'gut'
-    elif punkte >= 67:
+    elif prozent >= 67:
         return 'befriedigend'
-    elif punkte >= 50:
+    elif prozent >= 50:
         return 'ausreichend'
-    elif punkte >= 30:
+    elif prozent >= 30:
         return 'mangelhaft'
-    elif punkte <= 29 and punkte >= 0:
+    else:
         return 'ungenügend'
--- test_notenrechner.py
+++ test_notenrechner.py
@@ -1,61 +1,146 @@
 import pytest
-from notenrechner import berechne_note
+from notenrechner import berechne_note, berechne_note_prozent
 
+## Punkte und maximale Punkte eingabe
 # Positivtests
 def test_berechne_note__1():
     # Arrange
-    testwert = 100
-    soll_ergebnis = 'sehr gut'
+    punkte = 100
+    max_punkte = 100
+    soll_ergebnis = 100
 
     # Act
-    ist_ergebnis = berechne_note(testwert)
+    ist_ergebnis = berechne_note(punkte, max_punkte)
 
     # Assert
     assert ist_ergebnis == soll_ergebnis
 
 def test_berechne_note__3():
     # Arrange
-    testwert = 79
-    soll_ergebnis = 'befriedigend'
+    punkte = 60
+    max_punkte = 80
+    soll_ergebnis = 75
 
     # Act
-    ist_ergebnis = berechne_note(testwert)
+    ist_ergebnis = berechne_note(punkte, max_punkte)
 
     # Assert
     assert ist_ergebnis == soll_ergebnis
 
 def test_berechne_note__6():
     # Arrange
-    testwert = 0
-    soll_ergebnis = 'ungenügend'
+    punkte = 0
+    max_punkte = 6
+    soll_ergebnis = 0
 
     # Act
-    ist_ergebnis = berechne_note(testwert)
+    ist_ergebnis = berechne_note(punkte, max_punkte)
 
     # Assert
     assert ist_ergebnis == soll_ergebnis
 
 # Negativtests
-def test_berechne_note__ueber_100_pkt():
+def test_berechne_note__ueber_max_pkt():
     # Arrange
-    testwert = 101
+    punkte = 101
+    max_punkte = 100
 
     # Act
     with pytest.raises(ValueError):
-        berechne_note(testwert)
+        berechne_note(punkte, max_punkte)
 
 def test_berechne_note__unter_0_pkt():
     # Arrange
-    testwert = -1
+    punkte = -1
+    max_punkte = 50
 
     # Act
     with pytest.raises(ValueError):
-        berechne_note(testwert)
+        berechne_note(punkte, max_punkte)
 
 def test_berechne_note__type_error():
     # Arrange
-    testwert = 'test'
+    punkte = 'test'
+    max_punkte = 100
 
     # Act
     with pytest.raises(ValueError):
-        berechne_note(testwert)
+        berechne_note(punkte, max_punkte)
+
+def test_berechne_note__max_punkte_0():
+    # Arrange
+    punkte = 50
+    max_punkte = 0
+
+    # Act
+    with pytest.raises(ValueError):
+        berechne_note(punkte, max_punkte)
+
+def test_berechne_note__max_punkte_negativ():
+    # Arrange
+    punkte = 50
+    max_punkte = -100
+
+    # Act
+    with pytest.raises(ValueError):
+        berechne_note(punkte, max_punkte)
+
+## Prozentwerteingabe
+# Positivtests
+def test_berechne_note_prozent__1():
+    # Arrange
+    prozent = 100
+    soll_ergebnis = 'sehr gut'
+
+    # Act
+    ist_ergebnis = berechne_note_prozent(prozent)
+
+    # Assert
+    assert ist_ergebnis == soll_ergebnis
+
+def test_berechne_note_prozent__2():
+    # Arrange
+    prozent = 91
+    soll_ergebnis = 'gut'
+
+    # Act
+    ist_ergebnis = berechne_note_prozent(prozent)
+
+    # Assert
+    assert ist_ergebnis == soll_ergebnis
+
+def test_berechne_note_prozent__6():
+    # Arrange
+    prozent = 0
+    soll_ergebnis = 'ungenügend'
+
+    # Act
+    ist_ergebnis = berechne_note_prozent(prozent)
+
+    # Assert
+    assert ist_ergebnis == soll_ergebnis
+
+# Negativtests
+def test_berechne_note_prozent__ueber_100():
+    # Arrange
+    prozent = 101
+
+    # Act
+    with pytest.raises(ValueError):
+        berechne_note_prozent(prozent)
+
+def test_berechne_note_prozent__unter_0():
+    # Arrange
+    prozent = -1
+
+    # Act
+    with pytest.raises(ValueError):
+        berechne_note_prozent(prozent)
+
+def test_berechne_note_prozent__type_error():
+    # Arrange
+    prozent = 'test'
+
+    # Act
+    with pytest.raises(ValueError):
+        berechne_note_prozent(prozent)
